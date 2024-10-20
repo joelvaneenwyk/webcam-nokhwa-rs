1380399d62ab01319f672a30241a80d15006b711
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-10-20 15:14:19 -0700

    Fix examples/setting
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- examples/setting/src/main.rs
+++ examples/setting/src/main.rs
@@ -1,9 +1,24 @@
-use nokhwa::{Camera, CameraFormat, KnownCameraControl};
+use nokhwa::{utils::KnownCameraControl, Camera};
+use nokhwa::{
+    nokhwa_initialize,
+    pixel_format::RgbFormat,
+    query,
+    utils::{ApiBackend, RequestedFormat, RequestedFormatType},
+};
 
 fn main() {
-    let mut camera = Camera::new(0, None).unwrap();
+    // only needs to be run on OSX
+    nokhwa_initialize(|granted| {
+        println!("User said {}", granted);
+    });
+    let cameras = query(ApiBackend::Auto).unwrap();
+    cameras.iter().for_each(|cam| println!("{:?}", cam));
+
+    let format = RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
+    let first_camera = cameras.first().unwrap().index();
+
+    let mut camera = Camera::new(first_camera.clone(), format).unwrap();
     let known = camera.camera_controls_known_camera_controls().unwrap();
-    let mut control = *known.get(&KnownCameraControl::Gamma).unwrap();
-    control.set_value(101).unwrap();
-    camera.set_camera_control(control).unwrap();
+    let control = known.get(&KnownCameraControl::Gamma).unwrap();
+    camera.set_camera_control(control.control(), control.value()).unwrap();
 }
